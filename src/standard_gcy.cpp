5ee7edb0f0840425c7502c4d04eda9145a1a1c58
gcy3answer <2791544265@qq.com>
2026-03-28 11:36:23 +0800

    1 (#2)
    * 1
    
    * 1
    
    1
    
    * Update gimbal include path in standard_gcy.cpp
    
    * 2
    
    2
    
    * Update standard_gcy.cpp
    
    * Update logger.hpp
    
    * Update standard_gcy.cpp

--- src/standard_gcy.cpp
+++ src/standard_gcy.cpp
@@ -1,7 +1,97 @@
 #include <iostream>
 #include "fmt/format.h"
 
-int main() {
-    fmt::print("Hello from standard_gcy inside Dev Container!\n");
+#include <chrono>
+#include <opencv2/opencv.hpp>
+#include <thread>
+
+#include "io/camera.hpp"
+#include "io/dm_imu/dm_imu.hpp"
+#include "io/cboard_uart.hpp"
+#include "io/gimbal/gimbal.hpp"
+#include "io/command.hpp"
+
+#include "tasks/auto_aim/multithread/commandgener.hpp"
+#include "tasks/auto_aim/aimer.hpp"
+#include "tasks/auto_aim/shooter.hpp"
+#include "tasks/auto_aim/solver.hpp"
+#include "tasks/auto_aim/tracker.hpp"
+#include "tasks/auto_aim/yolo.hpp"
+
+#include "tools/exiter.hpp"
+#include "tools/img_tools.hpp"
+#include "tools/logger.hpp"
+#include "tools/math_tools.hpp"
+#include "tools/plotter.hpp"
+#include "tools/recorder.hpp"
+
+const std::string keys =
+  "{help h usage ? |      | 输出命令行参数说明}"
+  "{@config-path   | configs/standard3.yaml | 位置参数，yaml配置文件路径 }";
+
+
+
+int main(int argc, char * argv[]) {
+  fmt::print("Hello from standard_gcy inside Dev Container!\n");
+
+  cv::CommandLineParser cli(argc, argv, keys);
+  auto config_path = cli.get<std::string>(0);
+  if (cli.has("help") || config_path.empty()) {
+    cli.printMessage();
     return 0;
+  }
+
+
+  tools::Exiter exiter;
+  tools::Plotter plotter;
+
+  io::Gimbal gimbal(config_path);
+  
+  io::Camera camera(config_path);  
+  io::CBoardUART cboard(config_path);
+  
+  auto_aim::YOLO yolo(config_path, true);
+  auto_aim::Solver solver(config_path);
+  auto_aim::Tracker tracker(config_path, solver);
+  auto_aim::Shooter shooter(config_path);
+  auto_aim::Aimer aimer(config_path);
+
+
+  
+  cv::Mat img;  
+  Eigen::Quaterniond q; 
+  std::chrono::steady_clock::time_point t;
+  
+
+  auto mode = io::Mode::idle;   
+  auto last_mode = io::Mode::idle;  
+
+  while(!exiter.exit()){
+    camera.read(img,t);
+    q = cboard.imu_at(t - 1ms);
+    mode = cboard.mode;
+
+    
+    solver.set_R_gimbal2world(q);
+
+    Eigen::Vector3d final_xyz = tools::eulers(solver.R_gimbal2world(), 2, 1, 0);
+
+    auto armors = yolo.detect(img);
+
+    auto targets = tracker.track(armors, t);
+
+    auto cmd = aimer.aim(targets, t, cboard.bullet_speed);
+
+    gimbal.send(cmd);
+
+   
+
+    
+  }
+
+    
+   return 0;
 }
+
+    
+ 
